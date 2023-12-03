4a69063136404da44c4d7e09ff998ed9fbe4d797
VivianDee <vdagbue@gmail.com>
2023-12-03 00:42:07 +0000

    [add]: Searchbar for story tittles

--- africreads/africanlibrary/urls.py
+++ africreads/africanlibrary/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, stories, story_detail
+from .views import index, stories, story_detail, search
 
 urlpatterns = [
     path('', index, name="index"),
     path('stories/', stories, name="stories"),
     path('stories/<uuid:story_id>/', story_detail, name='story_detail'),
+    path('search/', search, name="search"),
 ]
--- africreads/africanlibrary/views.py
+++ africreads/africanlibrary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Story
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -21,3 +22,14 @@ def story_detail(request, story_id):
     story = get_object_or_404(Story, id=story_id)
     context = {'story': story}
     return render(request, 'story_detail.html', context)
+
+def search(request):
+    """ Searches the database for a story by name"""
+    query = request.GET.get('query', '')
+    stories = Story.objects.filter(title__icontains=query)
+
+    context = {'stories': stories, 'query': query}
+    count = len(stories.values())
+    if int(count) is 0:
+        messages.info(request, 'Your search {} did not match any stories'.format(query))
+    return render(request, 'stories.html', context)
